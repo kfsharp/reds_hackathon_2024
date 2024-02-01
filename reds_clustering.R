acffbd02757e47eb3ba1e58e78568b3fa0ab0b2d
kfsharp <kfsharp@uiowa.edu>
2024-01-31 23:05:19 -0600

    improved model and plots

--- reds_clustering.R
+++ reds_clustering.R
@@ -18,22 +18,15 @@ options(scipen = 999)
 pitch_data <- drop_read_csv("Datasets/Reds Hackathon/savant_pitch_level.csv")
 season_data <- drop_read_csv("Datasets/Reds Hackathon/fangraphs_season_level.csv")
 
-primary_roles <- pitch_data %>%
-  group_by(player_name) %>%
-  dplyr::summarize(
-    rp = sum(role_key == 'RP', na.rm = T),
-    sp = sum(role_key == 'SP', na.rm = T)
-  ) %>%
-  dplyr::mutate(
-    primary_role = ifelse(sp > rp, 1, 0)
-  )
-
 q_pitchers <- season_data %>%
   filter(IP >= 20) %>%
   group_by(MLBAMID, Season) %>%
-  select(NameASCII, MLBAMID, Season, Role, Location_plus, K_to_BB)
+  select(NameASCII, MLBAMID, Season, Location_plus, K_minus_BB_pct, Stuff_plus)
+
+pitch_data <- left_join(q_pitchers, primary_roles,by = c('NameASCII' = 'player_name'))
 
 pitch_counts <- pitch_data %>%
+  filter(!pitch_type %in% c('PO', 'FA', 'FO')) %>%
   group_by(game_year,player_name,pitcher,pitch_type) %>%
   dplyr::summarize(count = n())
 
@@ -43,21 +36,21 @@ variables <- pitch_data %>%
   group_by(game_year, player_name, pitcher) %>%
   dplyr::summarize(
     pitches = n(),
+    rp = sum(role_key == 'RP', na.rm = T),
+    sp = sum(role_key == 'SP', na.rm = T),
     arsenal = n_distinct(pitch_type[count >= 10]),
     mph_loss = cor(release_speed[pitch_type %in% c('FF', 'SI')], pitch_number_appearance[pitch_type %in% c('FF', 'SI')]),
-    platoon = mean(estimated_woba_using_speedangle[stand == 'R'], na.rm = T) - mean(estimated_woba_using_speedangle[stand == 'L'], na.rm = T)
+    primary_role = ifelse(sp > rp, 'SP', 'RP')
   ) %>%
-  filter(pitches >= 200)
-
-
-
+  filter(pitches >= 200) %>%
+  select(-c(rp, sp))
 
 merged_data <- left_join(q_pitchers, variables, by = c("MLBAMID" = "pitcher", "Season" = "game_year")) %>%
   select(-c('NameASCII')) %>%
   na.omit()
 
-# Create a binary outcome variable (1 for SP, 0 for RP)
-merged_data$role_binary <- as.factor(ifelse(merged_data$Role == "SP", 1, 0))
+#Create a binary outcome variable (1 for SP, 0 for RP)
+merged_data$role_binary <- as.factor(ifelse(merged_data$primary_role == "SP", 1, 0))
 
 df_train <- merged_data
 
@@ -66,21 +59,33 @@ df_train <- merged_data
 #######################################
 #Cross Validation
 addTaskCallback(function(...) {set.seed(123); TRUE})
-strata <- paste(df_train$Role, sep = "_")
+strata <- paste(df_train$role_binary, sep = "_")
 dt <- createDataPartition(y = strata, p = 0.7, list = FALSE)
 train <- df_train[dt,]
 test <- df_train[-dt,]
 
-# Build a logistic regression model
-model <- glm(role_binary ~ K_to_BB + Location_plus + mph_loss + arsenal + platoon, data = merged_data, family = "binomial")
-# save(model, file = "model_files/model_role_prob.RDS")
+#Build a logistic regression model
+model <- glm(role_binary ~ Stuff_plus + Location_plus + mph_loss + arsenal, data = merged_data, family = "binomial")
+#save(model, file = "model_files/model_role_prob.RDS")
 
-# Make predictions for role
+#Visualize Feature Importance
+feature_importance = caret::varImp(model)
+
+ggplot2::ggplot(feature_importance, aes(x=reorder(rownames(feature_importance),Overall), y=Overall)) +
+  geom_point( color="#FFCD00", size=4, alpha=0.6)+
+  geom_segment( aes(x=rownames(feature_importance), xend=rownames(feature_importance), y=0, yend=Overall), 
+                color='black') +
+  xlab('Variable')+
+  ylab('Overall Importance') +
+  theme_light() +
+  coord_flip() 
+
+#Make predictions for role
 test$role_prob <- predict(model, newdata = test, type = "response")
 
 #Find largest misfits
-rp_subset <- test[test$Role == "RP", ]
-sp_subset <- test[test$Role == "SP", ]
+rp_subset <- test[test$primary_role == "RP", ]
+sp_subset <- test[test$primary_role == "SP", ]
 
 #RP Misfits
 ten_largest_rp <- head(rp_subset[order(-rp_subset$role_prob), ], 10)
@@ -94,11 +99,11 @@ ten_smallest_sp <- head(sp_subset[order(sp_subset$role_prob), ], 10)
 df_23 <- merged_data %>%
   filter(Season == 2023)
 
-df_23$role_prob <- with(df_23, predict(model_role_prob, df_23, type = "response"))
+df_23$role_prob <- with(df_23, predict(model, df_23, type = "response"))
 
 #Find largest misfits
-rp_subset <- df_23[df_23$Role == "RP", ]
-sp_subset <- df_23[df_23$Role == "SP", ]
+rp_subset <- df_23[df_23$primary_role == "RP", ]
+sp_subset <- df_23[df_23$primary_role == "SP", ]
 
 #RP Misfits
 sp_candidates <- head(rp_subset[order(-rp_subset$role_prob), ], 10)
@@ -106,14 +111,55 @@ sp_candidates <- head(rp_subset[order(-rp_subset$role_prob), ], 10)
 rp_candidates <- head(sp_subset[order(sp_subset$role_prob), ], 10)
 
 
+#Batters per game appears to be bimodal distribution
+pitcher_role_types <- season_data %>%
+  group_by(NameASCII, Season) %>%
+  dplyr::summarize(
+    batters_faced = TBF/G
+  ) %>%
+  filter(Season == 2023)
 
+hist(pitcher_role_types$batters_faced) #Proof that there are only two roles for pitchers?
 
+#################################
+## Pitch Movement Plots
+pitcher_specific <- pitch_data %>%
+  filter(player_name == 'Phillips, Connor', game_year == 2023) %>%
+  tail(200)
 
+PitchTypeColors <- c("FF"="red", "SI"="deeppink", "FC"="blue", "CU"="orange", "KC"="orange", "SV"="orange", "SL"="deepskyblue", "ST"="deepskyblue", 
+                               "CH"="palegreen4", "FS"="palegreen3", "KN"="grey55")
 
+ggplot(pitcher_specific, aes(x = pfx_x*12, y = pfx_z*12)) + 
+  labs(title = paste0("Connor Phillips Pitch Movement (Last 200)")) + 
+  scale_x_continuous(limits = c(-30, 30),  breaks = c(-30, -20, -10, 0, 10, 20, 30)) + 
+  scale_y_continuous(limits = c(-30, 30),  breaks = c(-30, -20, -10, 0, 10, 20, 30)) +
+  geom_segment(aes(x = 0, y = -30, xend = 0, yend = 30), size = 1, color = "grey55") + 
+  geom_segment(aes(x = -30, y = 0, xend = 30, yend = 0), size = 1, color = "grey55") +
+  geom_point(aes(fill = pitch_type), color = "black", pch = 21, alpha = 0.8, size = 4, na.rm = TRUE) +
+  scale_fill_manual(values = PitchTypeColors) +
+  theme_bw() + theme(text = element_text(size = 12)) +
+  theme(plot.title = element_text(size = 16, face = "bold", hjust = 0.5), axis.text = element_text(size = 12), axis.title = element_blank()) + 
+  theme(legend.position = "none") +
+  coord_equal()
 
+## Pitch Locations
+get_background_info()
 
-
-
+ggplot2::ggplot(pitcher_specific, aes(x = plate_x, y = plate_z)) + 
+  scale_x_continuous(limits = c(-2.5,2.5)) + scale_y_continuous(limits = c(0,5)) +
+  geom_path(data = homePlateBatter, aes(x = x, y = y), size = 1) + 
+  geom_rect(aes(xmin = negZone - fullBall, xmax = posZone + fullBall, ymin = botZone - fullBall, ymax = topZone + fullBall), alpha = 0, size = 1, color = "black") +
+  geom_rect(aes(xmin = negZone , xmax = posZone, ymin = horzBottom, ymax = horzTop), alpha = 0, size = 1, color = "grey55") +
+  geom_rect(aes(xmin = vertLeft, xmax = vertRight, ymin = botZone, ymax = topZone), alpha = 0, size = 1, color = "grey55") +
+  geom_rect(aes(xmin = negZone, xmax = posZone, ymin = botZone, ymax = topZone), alpha = 0, size = 1, color = "black") +
+  geom_point(aes(fill = pitch_type), color = "black", pch = 21, alpha = 0.8, size = 4, na.rm = TRUE) +
+  scale_fill_manual(values = PitchTypeColors) +
+  theme_bw() + guides(fill = guide_legend(nrow = 1)) +
+  theme(text = element_text(size = 12), panel.grid = element_blank()) +
+  theme(plot.title = element_text(size = 14, face = "bold", hjust = 0.5)) +
+  theme(axis.text = element_blank(), axis.title = element_blank(), axis.ticks = element_blank()) + 
+  theme(legend.position = "bottom", legend.title = element_blank(), legend.text = element_text(size = 12))
 
 
 #########################################
