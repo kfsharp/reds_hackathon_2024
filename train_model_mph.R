a9c7cab2b59f2ec1aa4c951b0f7d00c7f95b9f10
BryerCashman <bryercashman@icloud.com>
2024-02-05 12:30:56 -0600

    found residuals for everyone

--- train_model_mph.R
+++ train_model_mph.R
@@ -16,10 +16,13 @@ load("model_files/model_role_prob.RDS")
 pitch_data <- drop_read_csv("Datasets/Reds Hackathon/savant_pitch_level.csv")
 season_data <- drop_read_csv("Datasets/Reds Hackathon/fangraphs_season_level.csv")
 
+
+
 q_pitchers <- season_data %>%
-  filter(IP >= 20) %>%
-  group_by(MLBAMID, Season) %>%
-  select(NameASCII, MLBAMID, Season, G)
+  dplyr::group_by(NameASCII,MLBAMID, Season) %>%
+  dplyr::summarize(IP = sum(IP),
+                   G = sum(G)) %>%
+  filter(IP >= 20) 
 
 pitch_counts <- pitch_data %>%
   filter(!pitch_type %in% c('PO', 'FA', 'FO')) %>%
@@ -29,18 +32,20 @@ pitch_counts <- pitch_data %>%
 pitch_data <- left_join(pitch_data, pitch_counts, by = c("game_year","player_name","pitcher","pitch_type"))
 
 variables <- pitch_data %>%
-  left_join(q_pitchers, by = c("MLBAMID" = "pitcher", "Season" = "game_year")) %>%
   group_by(game_year, player_name, pitcher) %>%
   dplyr::summarize(
     pitches = n(),
     mph_loss = cor(release_speed[pitch_type %in% c('FF', 'SI')], pitch_number_appearance[pitch_type %in% c('FF', 'SI')]),
-    fb_per_app = sum(pitch_type %in% c('FF', 'SI'), na.rm = T)/G
+    fbs = sum(pitch_type %in% c('FF', 'SI'), na.rm = T)
   ) %>%
   filter(pitches >= 200)
 
+
 merged_data <- left_join(q_pitchers, variables, by = c("MLBAMID" = "pitcher", "Season" = "game_year")) %>%
   select(-c('NameASCII')) %>%
-  na.omit()
+  na.omit() %>%
+  mutate(fbs_per_app = fbs/G)
+  
 
 df_train <- merged_data
 
@@ -49,12 +54,17 @@ df_train <- merged_data
 #######################################
 #Cross Validation
 addTaskCallback(function(...) {set.seed(123); TRUE})
-strata <- paste(df_train$role_binary, sep = "_")
+strata <- paste(df_train$mph_loss, sep = "_")
 dt <- createDataPartition(y = strata, p = 0.7, list = FALSE)
 train <- df_train[dt,]
 test <- df_train[-dt,]
 
 #Build a logistic regression model
-model <- lm(mph_loss ~ fb_per_app, data = merged_data)
+model <- lm(mph_loss ~ fbs_per_app, data = merged_data)
 summary(model)
 #save(model, file = "model_files/model_role_prob.RDS")
+
+merged_data$xmph_loss <- predict(model,merged_data)
+
+merged_data <- merged_data %>%
+  mutate(residual = mph_loss - xmph_loss)
